d5ab034486e20931f34321b0b9b797443d9d711c
mohamaddayoub <hamode.dayoub@gmail.com>
2023-03-30 18:03:29 +0300

    override instance type

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,12 +1,9 @@
 provider "kubernetes" {
-   
     host = module.myapp_eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.myapp_eks.cluster_certificate_authority_data)
-
     exec {
         api_version = "client.authentication.k8s.io/v1beta1"
-        command     = "aws"
-        
+        command = "aws" 
         args = ["eks", "get-token", "--cluster-name", module.myapp_eks.cluster_name]
     }
 }
@@ -49,8 +46,22 @@ module "myapp_eks" {
       name         = "worker-node-1"
       max_size     = 2
       desired_size = 1
+
       use_mixed_instances_policy = true
-      
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity = 0
+          on_demand_percentage_above_base_capacity = 10
+          spot_allocation_strategy = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "m5.large"
+            weighted_capacity = "1"
+          }
+        ]
+      }
     }
   }
 }
