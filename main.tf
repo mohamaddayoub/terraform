03f7adf5c50bb97cff7db0e5a3a50a804739adab
mohamaddayoub <hamode.dayoub@gmail.com>
2023-04-07 12:56:35 +0300

    updating

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "my_pub_key_location" {}
+
+
 resource "aws_vpc" "DevOps_VPC" {
     cidr_block = var.vpc_cidr_block
     tags = {
